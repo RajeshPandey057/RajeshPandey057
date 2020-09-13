2d2b902fafcb7b913c76bad6b3d734f01ca30abc
Rajesh Pandey <rpandey1498@gmail.com>
2020-09-13 22:12:23 +0530

    Auto Commit

--- GitAutoPush.bat
+++ GitAutoPush.bat
@@ -1,6 +1,8 @@
 #!/bin/sh
+C:\Users\rajesh\AppData\Local\Programs\Python\Python38\python.exe "D:\Professional\RajeshPandey057\Move.py"
 cd path D:\Professional\RajeshPandey057
 git add --all
 
-git commit -am "String Rotation "
+git commit -am "Auto Commit "
 git push
+pause
