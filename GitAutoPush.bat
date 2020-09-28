b6f018b94fc7f2dee616e1e8ab2bd1e58875679c
Rajesh Pandey <rpandey1498@gmail.com>
2020-09-28 09:56:35 +0530

    Auto Commit

--- GitAutoPush.bat
+++ GitAutoPush.bat
@@ -1,6 +1,4 @@
 C:\Users\rajesh\AppData\Local\Programs\Python\Python38\python.exe "D:\Professional\RajeshPandey057\Move.py"
-cd path D:\Professional\RajeshPandey057
 git add --all
-
 git commit -am "Auto Commit "
 git push
