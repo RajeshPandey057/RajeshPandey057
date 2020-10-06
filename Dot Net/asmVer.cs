bc4d4cfb1778eebccff42a72e93798f0c60eb9be
Rajesh Pandey <rpandey1498@gmail.com>
2020-10-06 10:35:12 +0530

    Auto Commit

---
+++ Dot Net/asmVer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temp
+{
+    public class class1
+    {
+        public static int ADD(int i, int j) => i + j;
+    }
+}
