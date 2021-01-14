5bb2fc1d66caa55fa28892c26e2af87e1f0281ee
RajeshPandey057 <rajesh-pandey@outlook.in>
2021-01-14 19:16:41 +0530

    Auto Commit

--- GitAutoPush.bat
+++ GitAutoPush.bat
@@ -1,12 +1,12 @@
-C:\Users\Rajesh\AppData\Local\Programs\Python\Python38\python.exe "D:\Professional\RajeshPandey057\Move.py"
+C:\Users\Rajesh\AppData\Local\Programs\Python\Python39\python.exe "D:\Professional\RajeshPandey057\Move.py"
 git add --all
 git commit -am "Auto Commit "
 git push
-C:\Users\Rajesh\AppData\Local\Programs\Python\Python38\python.exe "D:\Professional\RajeshPandey057\Move.py"
+C:\Users\Rajesh\AppData\Local\Programs\Python\Python39\python.exe "D:\Professional\RajeshPandey057\Move.py"
 git add --all
 git commit -am "Auto Commit "
 git push
-C:\Users\Rajesh\AppData\Local\Programs\Python\Python38\python.exe "D:\Professional\RajeshPandey057\Move.py"
+C:\Users\Rajesh\AppData\Local\Programs\Python\Python39\python.exe "D:\Professional\RajeshPandey057\Move.py"
 git add --all
 git commit -am "Auto Commit "
 git push
